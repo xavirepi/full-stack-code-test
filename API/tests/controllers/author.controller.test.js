54f894b5c97559f44bb75340249aa3a9f445399b
Javier Repi <xavirepi@hotmail.com>
2021-06-17 16:59:39 +0200

    create API endpoints response status tests

--- API/tests/controllers/author.controller.test.js
+++ API/tests/controllers/author.controller.test.js
@@ -13,8 +13,50 @@ beforeAll(async () => {
   await Author.create(authorsData);
 });
 
-it('get all authors', async () => {
-  const res = await request.get('/api/authors/')
+describe('Author Controller', () => {
+  // console.log('Authors Controller');
 
-  expect(res.status).toBe(200);
+  test('Get all authors', async () => {
+    // console.log('Get all authors list');
+    const res = await request.get('/api/authors/');
+
+    expect(res.status).toBe(200);
+    expect(res.status).not.toBeGreaterThanOrEqual(400);
+  });
+
+  test('Get one author', async () => {
+    // console.log('Get one author');
+    const id = "60c8dafc021c666acbf112b9"; // Author: Javier Repilado
+    const res = await request.get(`/api/author/${id}/`);
+
+    expect(res.status).toBe(200);
+    expect(res.status).not.toBeGreaterThanOrEqual(404);
+  });
+
+
+  test('Create one author', async () => {
+    const body = {
+      "first_name": 'Test_First_Name',
+      "last_name": 'Test_Last_Name'
+    }
+
+    const res = await request.post(`/api/author/`).send(body);
+
+    expect(res.status).toBe(201);
+    expect(res.status).not.toBeGreaterThanOrEqual(400);
+  });
+
+  test('Update one author', async () => {
+    const body = {
+      id: "60c8dafc021c666acbf112b9", // Author: Javier Repilado
+      first_name: 'Update_First_Name',
+      last_name: 'Update_Last_Name'
+    }
+
+    const res = await request.put(`/api/author/${body.id}`).send(body);
+
+    expect(res.status).toBe(201);
+    expect(res.status).not.toBeGreaterThanOrEqual(400);
+  });
+
 });
--- API/tests/controllers/book.controller.test.js
+++ API/tests/controllers/book.controller.test.js
@@ -6,16 +6,62 @@ const Book = require('../../models/Book.model');
 const Author = require('../../models/Author.model');
 const booksData = require('../../data/books.json');
 
-// afterAll(async () => {
-//   await mongoose.connection.close();
-// });
+afterAll(async () => {
+  await mongoose.connection.close();
+});
 
-// beforeAll(async () => {
-//   await Book.create(booksData);
-// });
+beforeAll(async () => {
+  await Book.create(booksData);
+});
 
-it('get all books', async () => {
-  const res = await request.get('/api/books/')
+describe('Book Controller', () => {
 
-  expect(res.status).toBe(200);
+  test('Get all books', async () => {
+    const res = await request.get('/api/books/');
+
+    expect(res.status).toBe(200);
+    expect(res.status).not.toBeGreaterThanOrEqual(400);
+  });
+
+  test('Get one book', async () => {
+    const id = "60c8dafd021c666acbf112cd"; //
+    const res = await request.get(`/api/book/${id}/`);
+
+    expect(res.status).toBe(200);
+    expect(res.status).not.toBeGreaterThanOrEqual(404);
+  });
+
+
+  test('Create one book', async () => {
+    const body = {
+      "name": "The Lord Of The Rings",
+      "isbn": "454-358-0384",
+      //"author": {
+        "first_name": "J.R.R.",
+        "last_name": "Tolkien",
+      //}
+    }
+
+    const res = await request.post(`/api/book/`).send(body);
+
+    expect(res.status).toBe(201);
+    expect(res.status).not.toBeGreaterThanOrEqual(400);
+  });
+
+  test('Update one book', async () => {
+    const body = {
+      "name": "The Hobbit",
+      "isbn": "454-358-0549",
+      "author": {
+        "first_name": "J.R.R.",
+        "last_name": "Tolkien",
+      },
+    }
+
+    const res = await request.put(`/api/book/${body.id}`).send(body);
+
+    expect(res.status).toBe(201);
+    expect(res.status).not.toBeGreaterThanOrEqual(400);
+  });
+
 });
